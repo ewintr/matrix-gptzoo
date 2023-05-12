a9751511e3f7dae7e62eb9d6ccfae03a7f70cf67
Erik Winter <e@ewintr.nl>
2023-05-12 15:07:16 +0200

    switched to mautrix example

--- go.mod
+++ go.mod
@@ -3,8 +3,23 @@ module ewintr.nl/matrix-bots
 go 1.20
 
 require (
-	github.com/matrix-org/gomatrix v0.0.0-20220926102614-ceba4d9f7530
-	github.com/russross/blackfriday/v2 v2.1.0
+	github.com/chzyer/readline v1.5.1
+	github.com/rs/zerolog v1.29.1
 	github.com/sashabaranov/go-openai v1.9.4
 	golang.org/x/exp v0.0.0-20230510235704-dd950f8aeaea
+	maunium.net/go/mautrix v0.15.1
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/tidwall/gjson v1.14.4 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
+	github.com/tidwall/sjson v1.2.5 // indirect
+	github.com/yuin/goldmark v1.5.4 // indirect
+	golang.org/x/crypto v0.8.0 // indirect
+	golang.org/x/net v0.9.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
+	maunium.net/go/maulogger/v2 v2.4.1 // indirect
 )
--- main.go
+++ main.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
-	"github.com/matrix-org/gomatrix"
-	"github.com/russross/blackfriday/v2"
+	"github.com/chzyer/readline"
+	"github.com/rs/zerolog"
 	"github.com/sashabaranov/go-openai"
 	"golang.org/x/exp/slog"
+	"maunium.net/go/mautrix"
+	"maunium.net/go/mautrix/event"
+	"maunium.net/go/mautrix/format"
+	"maunium.net/go/mautrix/id"
 	"os"
+	"os/signal"
+	"sync"
+	"time"
 )
 
 func main() {
@@ -16,11 +24,12 @@ func main() {
 		slog.Error("MATRIX_HOME_SERVER is not set")
 		os.Exit(1)
 	}
-	MatrixUserID, ok := os.LookupEnv("MATRIX_USER_ID")
+	MatrixUserIDStr, ok := os.LookupEnv("MATRIX_USER_ID")
 	if !ok {
 		slog.Error("MATRIX_USER_ID is not set")
 		os.Exit(1)
 	}
+	MatrixUserID := id.UserID(MatrixUserIDStr)
 	MatrixAccessKey, ok := os.LookupEnv("MATRIX_ACCESS_KEY")
 	if !ok {
 		slog.Error("MATRIX_ACCESS_KEY is not set")
@@ -35,21 +44,96 @@ func main() {
 	// Create new OpenAI client
 	openaiClient := openai.NewClient(OpenaiAPIKey)
 
-	matrixClient, _ := gomatrix.NewClient(MatrixHomeserver, MatrixUserID, MatrixAccessKey)
+	//matrixClient, _ := mautrix.NewClient(MatrixHomeserver, MatrixUserID, MatrixAccessKey)
+	//
+	//syncer := matrixClient.Syncer.(*mautrix.DefaultSyncer)
+	//syncer.OnEventType(event.StateMember, func(source mautrix.EventSource, evt *event.Event) {
+	//	if evt.Content["membership"] == "invite" && *ev.StateKey == MatrixUserID {
+	//		_, err := matrixClient.JoinRoom(ev.RoomID, "", nil)
+	//		if err != nil {
+	//			fmt.Println("Failed to join room:", err)
+	//		}
+	//	}
+	//})
+	//
+	//syncer.OnEventType("m.room.message", func(ev *gomatrix.Event) {
+	//	if ev.Sender != MatrixUserID {
+	//		msgBody, _ := ev.Body()
+	//
+	//		// Generate a message with OpenAI API
+	//		openAiResp, err := openaiClient.CreateChatCompletion(
+	//			context.Background(),
+	//			openai.ChatCompletionRequest{
+	//				Model: openai.GPT4,
+	//				Messages: []openai.ChatCompletionMessage{
+	//					{
+	//						Role:    openai.ChatMessageRoleSystem,
+	//						Content: "You are a chatbot that helps people by responding to their questions with short messages.",
+	//					},
+	//
+	//					{
+	//						Role:    openai.ChatMessageRoleUser,
+	//						Content: msgBody,
+	//					},
+	//				},
+	//			})
+	//
+	//		if err != nil {
+	//			fmt.Println("OpenAI API returned with ", err)
+	//			return
+	//		}
+	//
+	//		// Send the OpenAI response back to the chat
+	//		responseText := openAiResp.Choices[len(openAiResp.Choices)-1].Message.Content
+	//		formattedResponse := blackfriday.Run([]byte(responseText))
+	//		_, _ = matrixClient.SendFormattedText(ev.RoomID, responseText, string(formattedResponse))
+	//	}
+	//})
+	//
+	//for {
+	//	if err := matrixClient.Sync(); err != nil {
+	//		fmt.Println("Sync() returned with ", err)
+	//	}
+	//}
 
-	syncer := matrixClient.Syncer.(*gomatrix.DefaultSyncer)
-	syncer.OnEventType("m.room.member", func(ev *gomatrix.Event) {
-		if ev.Content["membership"] == "invite" && *ev.StateKey == MatrixUserID {
-			_, err := matrixClient.JoinRoom(ev.RoomID, "", nil)
-			if err != nil {
-				fmt.Println("Failed to join room:", err)
-			}
-		}
-	})
+	client, err := mautrix.NewClient(MatrixHomeserver, MatrixUserID, MatrixAccessKey)
+	if err != nil {
+		panic(err)
+	}
 
-	syncer.OnEventType("m.room.message", func(ev *gomatrix.Event) {
-		if ev.Sender != MatrixUserID {
-			msgBody, _ := ev.Body()
+	var oei mautrix.OldEventIgnorer
+	oei.Register(client.Syncer.(mautrix.ExtensibleSyncer))
+
+	rl, err := readline.New("[no room]> ")
+	if err != nil {
+		panic(err)
+	}
+	defer rl.Close()
+	log := zerolog.New(zerolog.NewConsoleWriter(func(w *zerolog.ConsoleWriter) {
+		w.Out = rl.Stdout()
+		w.TimeFormat = time.Stamp
+	})).With().Timestamp().Logger()
+	//if !*debug {
+	//	log = log.Level(zerolog.InfoLevel)
+	//}
+	client.Log = log
+
+	var lastRoomID id.RoomID
+
+	syncer := client.Syncer.(*mautrix.DefaultSyncer)
+
+	syncer.OnEventType(event.EventMessage, func(source mautrix.EventSource, evt *event.Event) {
+		lastRoomID = evt.RoomID
+		rl.SetPrompt(fmt.Sprintf("%s> ", lastRoomID))
+		log.Info().
+			Str("sender", evt.Sender.String()).
+			Str("type", evt.Type.String()).
+			Str("id", evt.ID.String()).
+			Str("body", evt.Content.AsMessage().Body).
+			Msg("Received message")
+
+		if evt.Sender != MatrixUserID {
+			msgBody := evt.Content.AsMessage().Body
 
 			// Generate a message with OpenAI API
 			openAiResp, err := openaiClient.CreateChatCompletion(
@@ -75,15 +159,92 @@ func main() {
 			}
 
 			// Send the OpenAI response back to the chat
-			responseText := openAiResp.Choices[len(openAiResp.Choices)-1].Message.Content
-			formattedResponse := blackfriday.Run([]byte(responseText))
-			_, _ = matrixClient.SendFormattedText(ev.RoomID, responseText, string(formattedResponse))
+			responseMarkdown := openAiResp.Choices[len(openAiResp.Choices)-1].Message.Content
+			responseMessage := format.RenderMarkdown(responseMarkdown, true, false)
+			client.SendMessageEvent(lastRoomID, event.EventMessage, &responseMessage)
+		}
+
+	})
+	syncer.OnEventType(event.StateMember, func(source mautrix.EventSource, evt *event.Event) {
+		if evt.GetStateKey() == client.UserID.String() && evt.Content.AsMember().Membership == event.MembershipInvite {
+			_, err := client.JoinRoomByID(evt.RoomID)
+			if err == nil {
+				lastRoomID = evt.RoomID
+				rl.SetPrompt(fmt.Sprintf("%s> ", lastRoomID))
+				log.Info().
+					Str("room_id", evt.RoomID.String()).
+					Str("inviter", evt.Sender.String()).
+					Msg("Joined room after invite")
+			} else {
+				log.Error().Err(err).
+					Str("room_id", evt.RoomID.String()).
+					Str("inviter", evt.Sender.String()).
+					Msg("Failed to join room after invite")
+			}
 		}
 	})
 
-	for {
-		if err := matrixClient.Sync(); err != nil {
-			fmt.Println("Sync() returned with ", err)
+	//cryptoHelper, err := cryptohelper.NewCryptoHelper(client, []byte("meow"), "mautrix-example.db")
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//// You can also store the user/device IDs and access token and put them in the client beforehand instead of using LoginAs.
+	////client.UserID = "..."
+	////client.DeviceID = "..."
+	////client.AccessToken = "..."
+	//// You don't need to set a device ID in LoginAs because the crypto helper will set it for you if necessary.
+	////cryptoHelper.LoginAs = &mautrix.ReqLogin{
+	////	Type:       mautrix.AuthTypePassword,
+	////	Identifier: mautrix.UserIdentifier{Type: mautrix.IdentifierTypeUser, User: *username},
+	////	Password:   *password,
+	////}
+	//// If you want to use multiple clients with the same DB, you should set a distinct database account ID for each one.
+	////cryptoHelper.DBAccountID = ""
+	//err = cryptoHelper.Init()
+	//if err != nil {
+	//	panic(err)
+	//}
+	//// Set the client crypto helper in order to automatically encrypt outgoing messages
+	//client.Crypto = cryptoHelper
+
+	log.Info().Msg("Now running")
+	syncCtx, cancelSync := context.WithCancel(context.Background())
+	var syncStopWait sync.WaitGroup
+	syncStopWait.Add(1)
+
+	go func() {
+		err = client.SyncWithContext(syncCtx)
+		defer syncStopWait.Done()
+		if err != nil && !errors.Is(err, context.Canceled) {
+			panic(err)
 		}
-	}
+	}()
+
+	done := make(chan os.Signal)
+	signal.Notify(done, os.Interrupt)
+	<-done
+
+	//for {
+	//	line, err := rl.Readline()
+	//	if err != nil { // io.EOF
+	//		break
+	//	}
+	//	if lastRoomID == "" {
+	//		log.Error().Msg("Wait for an incoming message before sending messages")
+	//		continue
+	//	}
+	//	resp, err := client.SendText(lastRoomID, line)
+	//	if err != nil {
+	//		log.Error().Err(err).Msg("Failed to send event")
+	//	} else {
+	//		log.Info().Str("event_id", resp.EventID.String()).Msg("Event sent")
+	//	}
+	//}
+	cancelSync()
+	syncStopWait.Wait()
+	//err = cryptoHelper.Close()
+	//if err != nil {
+	//	log.Error().Err(err).Msg("Error closing database")
+	//}
 }
