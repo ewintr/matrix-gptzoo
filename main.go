bfcba9a4640bfdf9f734be281c369fca10c8ea18
Erik Winter <e@ewintr.nl>
2023-06-13 19:29:54 +0200

    matching env secrets with config file

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"os/signal"
 
@@ -17,6 +18,35 @@ func main() {
 	if _, err := toml.DecodeFile(getParam("CONFIG_PATH", "conf.toml"), &config); err != nil {
 		logger.Error(err.Error())
 		os.Exit(1)
+	}
+	type Credentials struct {
+		Password  string
+		AccessKey string
+	}
+	credentials := make(map[string]Credentials)
+	for i := 0; i < len(config.Bots); i++ {
+		user := getParam(fmt.Sprintf("MATRIX_BOT%d_ID", i), "")
+		if user == "" {
+			logger.Error("missing user id", slog.Int("user", i))
+			os.Exit(1)
+		}
+		credentials[user] = Credentials{
+			Password:  getParam(fmt.Sprintf("MATRIX_BOT%d_PASSWORD", i), ""),
+			AccessKey: getParam(fmt.Sprintf("MATRIX_BOT%d_ACCESSKEY", i), ""),
+		}
+	}
+	for i, bc := range config.Bots {
+		creds, ok := credentials[bc.UserID]
+		if !ok {
+			logger.Error("missing credentials", slog.Int("user", i))
+			os.Exit(1)
+		}
+		config.Bots[i].UserPassword = creds.Password
+		config.Bots[i].UserAccessKey = creds.AccessKey
+	}
+
+	config.OpenAI = bot.ConfigOpenAI{
+		APIKey: getParam("OPENAI_API_KEY", ""),
 	}
 	logger.Info("loaded config", slog.Int("bots", len(config.Bots)))
 
